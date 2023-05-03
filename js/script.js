0d62767b1e08e8ee3d7c17440afd9e476882c9f1
LncN1939 <leo.corea13419@est.uca.edu.ni>
2023-05-02 21:04:36 -0600

    Solved bugs

--- js/script.js
+++ js/script.js
@@ -1,42 +1,39 @@
 const imagen1 = document.querySelector('#imagen1');
+const imagen2 = document.querySelector('#imagen2');
 const mensaje = document.querySelector('#mensaje');
 
+let lock = false;
+
 imagen1.addEventListener('click', () => {
+  if (lock) {
+    return;
+  }
+  lock = true;
+  console.log('1')
   const messageText = imagen1.getAttribute('data-message');
   const messageImage = imagen1.getAttribute('data-image');
   const messageHtml = `<div class="message-container"><div class="message-text"><h2>Información sobre el álbum</h2><p>${messageText}</p></div><div class="message-image"><img src="${messageImage}" /></div></div>`;
   mensaje.innerHTML = messageHtml;
   mensaje.classList.remove('oculto');
+  setTimeout(() => {
+    lock = false;
+  }, 100);
 });
-
-mensaje.addEventListener('click', () => {
-  mensaje.classList.add('oculto');
-});
-
-const imagen2 = document.querySelector('#imagen2');
-const mensaje2 = document.querySelector('#mensaje');
 
 imagen2.addEventListener('click', () => {
+  if (lock) {
+    return;
+  }
+  lock = true;
+  console.log('2')
   const messageText = imagen2.getAttribute('data-message');
   const messageImage = imagen2.getAttribute('data-image');
   const messageHtml = `<div class="message-container"><div class="message-text"><h2>Información sobre el álbum</h2><p>${messageText}</p></div><div class="message-image"><img src="${messageImage}" /></div></div>`;
   mensaje.innerHTML = messageHtml;
   mensaje.classList.remove('oculto');
-});
-
-mensaje.addEventListener('click', () => {
-  mensaje.classList.add('oculto');
-});
-
-const imagen3 = document.querySelector('#imagen3');
-const mensaje3 = document.querySelector('#mensaje');
-
-imagen3.addEventListener('click', () => {
-  const messageText = imagen3.getAttribute('data-message');
-  const messageImage = imagen3.getAttribute('data-image');
-  const messageHtml = `<div class="message-container"><div class="message-text"><h2>Información sobre el álbum</h2><p>${messageText}</p></div><div class="message-image"><img src="${messageImage}" /></div></div>`;
-  mensaje.innerHTML = messageHtml;
-  mensaje.classList.remove('oculto');
+  setTimeout(() => {
+    lock = false;
+  }, 100);
 });
 
 mensaje.addEventListener('click', () => {
